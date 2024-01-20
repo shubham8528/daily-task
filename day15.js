53b90939dad72f16ade16afa8bcebb30ec5981dd
shubham bisht <shubham.bisht@etelligens.in>
2024-01-20 20:25:55 +0530

    day 15 completed

---
+++ day15.js
@@ -0,0 +1,14 @@
+var twoSum = function (nums, target) {
+    let index
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target)
+
+                index = [i, j]
+        }
+    }
+    return index
+};
+
+console.log(twoSum([3, 2, 4], 6))
+console.log(twoSum([2, 7, 11, 15], 9))
